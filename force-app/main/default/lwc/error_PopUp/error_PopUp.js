388d2c31afcf97ae588b782076218333b3d4e907
Mitrajsinh Gohil <mitraj.g@mvclouds.com>
2024-06-13 14:40:53 +0530

    commit after adding comments

--- force-app/main/default/lwc/error_PopUp/error_PopUp.js
+++ force-app/main/default/lwc/error_PopUp/error_PopUp.js
@@ -4,22 +4,28 @@ import errorImage from '@salesforce/resourceUrl/ErrorIcons'
 export default class Error_PopUp extends LightningElement {
     @track isError = false;
     @track isSuccess = false;
-    @track isWarning = true;
+    @track isWarning = false;
     @track iconUrl = errorImage + '/ErrorIcons/ErrorIcon.png';
     backGroundUrl = errorImage + '/ErrorIcons/ErrorPopUpBg.png';
 
-    connectedCallback(){
-        if(this.isSuccess){
-            this.iconUrl = errorImage + '/ErrorIcons/SuccessIcon.png';
-            this.isSuccess = true;
-        }else if(this.isError){
-            this.isError = true;
-        }else if(this.isWarning){
-            this.isWarning = true;
-            this.iconUrl = errorImage + '/ErrorIcons/WarningIcon.png';
-        }
-    };
+    // connectedCallback(){
+    //     if(this.isSuccess){
+    //         this.iconUrl = errorImage + '/ErrorIcons/SuccessIcon.png';
+    //         this.isSuccess = true;
+    //     }else if(this.isError){
+    //         this.isError = true;
+    //     }else if(this.isWarning){
+    //         this.isWarning = true;
+    //         this.iconUrl = errorImage + '/ErrorIcons/WarningIcon.png';
+    //     }
+    // };
 
+    /**
+    * Method Name: showToast
+    * @description: Method to display error, warning and success message as per conditions
+    * Date: 12/06/2024
+    * Created By: Mitrajsinh Gohil
+    */
     @api
     showToast(type, message, title){
         try {
@@ -41,7 +47,12 @@ export default class Error_PopUp extends LightningElement {
         }
     }
     
-
+    /**
+    * Method Name: copyCode
+    * @description: method to copy the unique code of error
+    * Date: 12/06/2024
+    * Created By: Mitrajsinh Gohil
+    */
     copyCode() {
         // Copy code to clipboard
         var range = document.createRange();
@@ -52,6 +63,12 @@ export default class Error_PopUp extends LightningElement {
         window.getSelection().removeAllRanges();// to deselect
     }
 
+    /**
+    * Method Name: cancel
+    * @description: Method for closing the pop up using the custom event
+    * Date: 12/06/2024
+    * Created By: Mitrajsinh Gohil
+    */
     cancel(){
         //Handle Cancel action
         console.log('Cancel button clicked');
@@ -59,16 +76,34 @@ export default class Error_PopUp extends LightningElement {
         this.dispatchEvent(event1);
     }
 
+    /**
+    * Method Name: support
+    * @description: Method for support redirect from the pop up using the custom event
+    * Date: 12/06/2024
+    * Created By: Mitrajsinh Gohil
+    */
     support() {
         // Handle support action
         console.log('Support button clicked');
     }
 
+    /**
+    * Method Name: knowledge
+    * @description: Method for knowledge redirect from the pop up using the custom event
+    * Date: 12/06/2024
+    * Created By: Mitrajsinh Gohil
+    */
     knowledge() {
         // Handle knowledge action
         console.log('Knowledge button clicked');
     }
     
+    /**
+    * Method Name: resume
+    * @description: Method for resuming the task and closing the pop up using the custom event
+    * Date: 12/06/2024
+    * Created By: Mitrajsinh Gohil
+    */
     resume() {
         // Handle knowledge action
         console.log('Knowledge button clicked');
