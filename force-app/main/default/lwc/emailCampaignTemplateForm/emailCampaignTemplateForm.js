85c082cf9ceec5d47290df588e009509dc11b15d
Rachit Shah <rachitshah2893@gmail.com>
2024-06-20 16:12:57 +0530

    Added more css changes

--- force-app/main/default/lwc/emailCampaignTemplateForm/emailCampaignTemplateForm.js
+++ force-app/main/default/lwc/emailCampaignTemplateForm/emailCampaignTemplateForm.js
@@ -12,7 +12,7 @@ export default class EmailCampaignTemplateForm extends LightningElement {
         { label: 'Sending emails on specific dates', value: 'specificDates' },
     ];
     @track startDateOption = 'specificDate';
-    @track dripEmails = [
+    @track emails = [
         { id: 1, name: 'Buyer - First Time', subject: 'Finding the Right Home', daysAfterStartDate: 6, timeToSend: '10:59 AM' },
         { id: 2, name: 'Buyer - First Time', subject: 'Finding the Right Financing', daysAfterStartDate: 5, timeToSend: '03:59 AM' },
     ];
@@ -26,13 +26,13 @@ export default class EmailCampaignTemplateForm extends LightningElement {
     }
 
     handleAddNewEmail() {
-        const newId = this.dripEmails.length + 1;
-        this.dripEmails = [...this.dripEmails, { id: newId, name: 'New Email', subject: '', daysAfterStartDate: 0, timeToSend: '' }];
+        const newId = this.emails.length + 1;
+        this.emails = [...this.emails, { id: newId, name: 'New Email', subject: '', daysAfterStartDate: 0, timeToSend: '' }];
     }
 
     handleDeleteEmail(event) {
         const emailId = event.currentTarget.dataset.id;
-        this.dripEmails = this.dripEmails.filter(email => email.id !== parseInt(emailId, 10));
+        this.emails = this.emails.filter(email => email.id !== parseInt(emailId, 10));
     }
 
     handleCancel() {
@@ -41,5 +41,9 @@ export default class EmailCampaignTemplateForm extends LightningElement {
 
     handleSave() {
         // Handle save logic
+    }
+
+    handleEdit(){
+        console.log('Edit button is clicked');
     }
 }
