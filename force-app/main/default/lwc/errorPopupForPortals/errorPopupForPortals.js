9452539b594e9e3146816a68ce46b461715608b9
Karan3036 <karsingh9377@gmail.com>
2024-07-12 17:00:26 +0530

    EXAPP-164 Removed the unwanted css.

--- force-app/main/default/lwc/errorPopupForPortals/errorPopupForPortals.js
+++ force-app/main/default/lwc/errorPopupForPortals/errorPopupForPortals.js
@@ -30,42 +30,6 @@ export default class ErrorPopupForPortals extends LightningElement {
     }
     
     /**
-    * Method Name: renderedCallback
-    * @description: Used to set the style.
-    * Date: 09/07/2024
-    * Created By: Karan Singh
-    **/
-    renderedCallback() {
-        try {
-            if (this.isInitalRender) {
-                const body = document.querySelector("body");
-
-                const style = document.createElement('style');
-                style.innerText = `
-                        .popup__body .popup__custom_card article {
-                            display: contents;
-                        }
-
-                        .closeIcon .slds-icon-text-default {
-                            height: 25px;
-                            width: 25px;
-                        }
-
-                        .popup__custom_card .slds-card__header{
-                            display: none;
-                        }
-                `;
-
-                body.appendChild(style);
-                this.isInitalRender = false;
-            }
-        } catch (error) {
-            console.log(' error in render : ', error.messsage);
-
-        }
-    }
-
-    /**
     * Method Name: handleCloseModal
     * @description: Used to close the popup.
     * Date: 09/07/2024
