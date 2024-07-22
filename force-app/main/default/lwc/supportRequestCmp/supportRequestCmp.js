a90f4577c144bcbbde5abcfa79281e4e64d74996
vyomsoni <vyom58ppsv2020@gmail.com>
2024-07-22 18:21:33 +0530

    EXAPP-233-add validation and set mobile view

--- force-app/main/default/lwc/supportRequestCmp/supportRequestCmp.js
+++ force-app/main/default/lwc/supportRequestCmp/supportRequestCmp.js
@@ -5,46 +5,78 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 export default class SupportRequestCmp extends LightningElement {
 
     @track supportname = '';
-    email ='';
-    subject = '';
-    message = '';
-    name_msg = true;
-    email_msg = true;
-    Message_msg = true;
-    subject_msg = true;
+    @track email ='';
+    @track subject = '';
+    @track message = '';
+    @track name_msg = true;
+    @track email_msg = true;
+    @track message_msg = true;
+    @track subject_msg = true;
     @track filesData = [];
-    @track FName = [];
+    @track FName = [];     
     @track FBase64 = [];
     @track totalsize = parseInt(0);
     @track filename;
     @track filedata;
 
+    /**
+    * Method Name : Support_name
+    * @description : set the support name value
+    * date:22/07/2024
+    * Created By: Vyom Soni
+    */
     Support_name(event) {
         this.supportname = event.target.value;
         this.supportname = this.supportname.trim();
         this.name_msg = true;
-      }
+    }
 
-      Support_email(event) {
-        this.email = event.target.value;
-        this.email_msg = true;
-      }
-      Support_message(event) {
-        this.message = event.target.value;
-        this.message = this.message.trim();
-        this.Message_msg = true;
-    
-      }
-      Support_subject(event) {
-        this.subject = event.target.value;
-        this.subject = this.subject.trim();
-        this.subject_msg = true;
-    
-      }
-      onSubmit() {
-        this.spinnerdatatable = true;
-        var pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        var validation1 = pattern.test(this.email);
+
+    /**
+    * Method Name : Support_email
+    * @description : set the support email value
+    * date:22/07/2024
+    * Created By: Vyom Soni
+    */
+    Support_email(event) {
+      this.email = event.target.value;
+      this.email_msg = true;
+    }
+
+     /**
+    * Method Name : Support_message
+    * @description : set the support message value
+    * date:22/07/2024
+    * Created By: Vyom Soni
+    */
+    Support_message(event) {
+      this.message = event.target.value;
+      this.message = this.message.trim();
+      this.message_msg = true;
+    }
+
+     /**
+    * Method Name : Support_subject
+    * @description : set the support subject value
+    * date:22/07/2024
+    * Created By: Vyom Soni
+    */
+    Support_subject(event) {
+      this.subject = event.target.value;
+      this.subject = this.subject.trim();
+      this.subject_msg = true;
+    }
+      
+      /**
+    * Method Name : onSubmit
+    * @description : handle the from submit button
+    * date:22/07/2024
+    * Created By: Vyom Soni
+    */
+    onSubmit() {
+      this.spinnerdatatable = true;
+      var pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      var validation1 = pattern.test(this.email);
         if (this.supportname == undefined || this.supportname == '') {
           this.name_msg = false;
           this.spinnerdatatable = false;
@@ -55,58 +87,61 @@ export default class SupportRequestCmp extends LightningElement {
           this.subject_msg = false;
           this.spinnerdatatable = false;
         } else if (this.message == undefined || this.message == '') {
-          this.Message_msg = false;
+          this.message_msg = false;
           this.spinnerdatatable = false;
         } else {
           this.email_msg = true;
-          sendemail({
-            name: this.supportname,
-            email: this.email,
-            subject: this.subject,
-            body: this.message,
-            fname: this.FName,
-            fbase64: this.FBase64
-          })
-            .then(result => {
-              if (result == 'success') {
-                this.spinnerdatatable = false;
-                this.supportname = '';
-                this.email = '';
-                this.message = '';
-                this.subject = '';
-                this.filesData = [];
-                const event = new ShowToastEvent({
-                  title: 'Success',
-                  message: 'Email Sent Successfully.',
-                  variant: 'success',
-                });
-                this.dispatchEvent(event);
-              } else {
-                this.spinnerdatatable = false;
-                const event = new ShowToastEvent({
-                  title: 'Error',
-                  message: 'An error occurred while sending Email.',
-                  variant: 'error',
-                });
-                this.dispatchEvent(event);
-              }
-            })
-    
-    
+          this.sendEmailCallMethod();
         }
-    
-      }
-      onClear() {
-        this.supportname = '';
-        this.email = '';
-        this.message = '';
-        this.subject = '';
-        this.name_msg = true;
-        this.email_msg = true;
-        this.subject_msg = true;
-        this.Message_msg = true;
-        this.filesData = [];
-      }
+    }
+
+      /**
+    * Method Name : sendEmailCallMethod
+    * @description : call the apex method for the send email.
+    * date:22/07/2024
+    * Created By: Vyom Soni
+    */
+    sendEmailCallMethod(){
+      sendemail({
+          name: this.supportname,
+          email: this.email,
+          subject: this.subject,
+          body: this.message,
+          fname: this.FName,
+          fbase64: this.FBase64
+        })
+          .then(result => {
+            if (result == 'success') {
+              this.spinnerdatatable = false;
+              this.supportname = '';
+              this.email = '';
+              this.message = '';
+              this.subject = '';
+              this.filesData = [];
+              const event = new ShowToastEvent({
+                title: 'Success',
+                message: 'Email Sent Successfully.',
+                variant: 'success',
+              });
+              this.dispatchEvent(event);
+            } else {
+              this.spinnerdatatable = false;
+              const event = new ShowToastEvent({
+                title: 'Error',
+                message: 'An error occurred while sending Email.',
+                variant: 'error',
+              });
+              this.dispatchEvent(event);
+            }
+         })
+    }
+
+      /**
+      * Method Name : handleUploadFinished
+      * @description : handle the file uploader onchange event check file size for the upload
+      * date:22/07/2024
+      * Created By: Vyom Soni
+      */
       handleUploadFinished(event) {
         if (event.target.files.length > 0) {
           for (let i = 0; i < event.target.files.length; i++) {
@@ -140,6 +175,13 @@ export default class SupportRequestCmp extends LightningElement {
           }
         }
       }
+
+      /**
+      * Method Name : removeReceiptImage
+      * @description : handle the remove the images from the image list
+      * date:22/07/2024
+      * Created By: Vyom Soni
+      */
       removeReceiptImage(event) {
         var index = event.currentTarget.dataset.id;
         var binaryString = atob(this.FBase64[index]);
