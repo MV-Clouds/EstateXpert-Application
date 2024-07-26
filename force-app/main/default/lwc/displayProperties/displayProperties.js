51ee36979480fa1a5ef3d87aebb2f7f214dbbdc3
Rachit Shah <rachitshah2893@gmail.com>
2024-07-26 17:06:22 +0530

    Added comment and do some css changes

--- force-app/main/default/lwc/displayProperties/displayProperties.js
+++ force-app/main/default/lwc/displayProperties/displayProperties.js
@@ -6,10 +6,8 @@ import propertyIcons from '@salesforce/resourceUrl/PropertyIcons';
 import location_icon from '@salesforce/resourceUrl/location_icon';
 import mapCss_V1 from '@salesforce/resourceUrl/mapCss_V1';
 import { loadStyle } from 'lightning/platformResourceLoader';
-import getListingTypes from '@salesforce/apex/PropertySearchController.getListingTypes';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { CurrentPageReference } from 'lightning/navigation';
-// const PAGE_SIZE = 6;
 
 export default class DisplayProperties extends NavigationMixin(LightningElement) {
     @api recordId;
@@ -29,7 +27,6 @@ export default class DisplayProperties extends NavigationMixin(LightningElement)
     };
     
     @track isLoading = false;
-
     @track pageSize = 6;
     @track bathroom_icon = propertyIcons + '/PropertyIcons/Bathroom.png';
     @track bedroom_icon = propertyIcons + '/PropertyIcons/Bedroom.png';
@@ -40,7 +37,6 @@ export default class DisplayProperties extends NavigationMixin(LightningElement)
     @track propertyMediaUrls;
     @track isPropertyAvailable = true;
     @track selectedView = 'Grid'; 
-    @track listingTypeOptions = [];
     @track isInitial = false;
 
     get isFilterButtonDisabled() {
@@ -60,7 +56,6 @@ export default class DisplayProperties extends NavigationMixin(LightningElement)
         return this.isMapView ? 'slds-button slds-button_brand' : 'slds-button slds-button_neutral';
     }
 
-    
     get isListView() {
         return this.selectedView === 'List';
     }
@@ -120,12 +115,18 @@ export default class DisplayProperties extends NavigationMixin(LightningElement)
         return null;
     }
 
+    /**
+    * Method Name: ConnectedCallback
+    * @description: Standard ConnectedCallback method which executes when the component is loaded and it is calling apex to fetch all the properties and loading map library
+    * Date: 17/06/2024
+    * Created By: Mitrajsinh Gohil
+    * Last modified by : Rachit Shah
+    */
     connectedCallback() {
         console.log('Object Name ==> ' , this.objectName);
         console.log('recordId => ' , this.recordId);
         this.isLoading = true;
         this.fetchListings();
-        this.fetchListingTypes();
         loadStyle(this, mapCss_V1)
             .then(() =>{
                 console.log('CSS loaded');
@@ -137,6 +138,13 @@ export default class DisplayProperties extends NavigationMixin(LightningElement)
             });
     }
 
+    /**
+    * Method Name: fetchListings
+    * @description: this method is used to get all properties data from the apex and update the property list to display them
+    * Date: 17/06/2024
+    * Created By: Mitrajsinh Gohil
+    * Last modified by : Rachit Shah
+    */
     fetchListings() {
         getListings({ recordId: this.recordId , objectName : this.objectName})
             .then(result => {
@@ -172,7 +180,7 @@ export default class DisplayProperties extends NavigationMixin(LightningElement)
                     const listingBathroom = listing.Bathrooms__c;
                     this.filterData.bedrooms = listingBedroom;
                     this.filterData.bathrooms = listingBathroom;
-                    this.applyFilters();
+                    this.applyFilters(true);
                 }
 
                 else if(this.objectName == 'Inquiry__c'){
@@ -181,21 +189,20 @@ export default class DisplayProperties extends NavigationMixin(LightningElement)
                     const listingBathroom = inquiry.Bedrooms_min__c;
                     this.filterData.bedrooms = listingBedroom;
                     this.filterData.bathrooms = listingBathroom;
-                    this.applyFilters();
+                    this.applyFilters(true);
                 }
                 this.updateMapMarkers();
             })
             .catch(error => console.error('Error getting properties from apex:', error));
     }
 
-    fetchListingTypes() {
-        getListingTypes()
-            .then(result => {
-                this.listingTypeOptions = result.map(type => ({ label: type, value: type }));
-            })
-            .catch(error => console.error(error));
-    }
-
+    /**
+    * Method Name: updateMapMarkers
+    * @description: this method is used to update and set the markers on the map for the properties with the pagination
+    * Date: 17/06/2024
+    * Created By: Mitrajsinh Gohil
+    * Last modified by : Rachit Shah
+    */
     updateMapMarkers() {
 
         const startIndex = (this.currentPage - 1) * this.pageSize;
@@ -217,12 +224,24 @@ export default class DisplayProperties extends NavigationMixin(LightningElement)
 
     }
 
+    /**
+    * Method Name: handleInputChange
+    * @description: this method is used to save the changes of data
+    * Date: 25/07/2024
+    * Created By: Rachit Shah
+    */
     handleInputChange(event) {
         const field = event.target.dataset.field;
         this.filterData[field] = event.target.value;
     }
 
-    applyFilters() {
+    /**
+    * Method Name: applyFilters
+    * @description: this method is used apply filter
+    * Date: 25/07/2024
+    * Created By: Rachit Shah
+    */
+    applyFilters(showToast) {
         try {
             const { listingTypes, minPrice, maxPrice, bedrooms, bathrooms, city, zipcode } = this.filterData;
     
@@ -246,7 +265,7 @@ export default class DisplayProperties extends NavigationMixin(LightningElement)
     
             this.isPropertyAvailable = this.pagedFilteredListingData.length > 0;
             this.currentPage = 1;
-            if(!this.searchTerm && !this.isInitial){
+            if(!this.searchTerm && !this.isInitial && showToast){
                 this.showToast('Success', 'Filter applied successfully', 'success');
             }
 
@@ -262,7 +281,12 @@ export default class DisplayProperties extends NavigationMixin(LightningElement)
         }
     }
     
-
+    /**
+    * Method Name: clearFilter
+    * @description: this method is used to clear filter
+    * Date: 25/07/2024
+    * Created By: Rachit Shah
+    */
     clearFilter() {
         this.filterData = {
             city: '',
@@ -283,31 +307,61 @@ export default class DisplayProperties extends NavigationMixin(LightningElement)
         this.updateMapMarkers();
     }
 
+    /**
+    * Method Name: handleSearch
+    * @description: this method is used to filter the properties based on the search key without overriding other filters
+    * Date: 17/06/2024
+    * Created By: Mitrajsinh Gohil
+    */ 
     handleSearch(event) {
         this.searchTerm = event.target.value.toLowerCase();
-        this.applyFilters();
-        this.currentPage = 1; // Reset to first page on search
+        this.applyFilters(false);
+        this.currentPage = 1;
         this.totalRecords = this.pagedFilteredListingData.length;
         this.isPropertyAvailable = this.totalRecords > 0;
         this.updateMapMarkers();
     }
 
+    /**
+    * Method Name: setGridView
+    * @description: this method is used to set grid view
+    * Date: 17/06/2024
+    * Created By: Mitrajsinh Gohil
+    */
     setGridView() {
         this.selectedView = 'Grid';
         this.currentPage = 1;
     }
 
+    /**
+    * Method Name: setListView
+    * @description: this method is used to set list view
+    * Date: 17/06/2024
+    * Created By: Mitrajsinh Gohil
+    */
     setListView() {
         this.selectedView = 'List';
         this.currentPage = 1;
     }
 
+    /**
+    * Method Name: setMapView
+    * @description: this method is used to set map view
+    * Date: 17/06/2024
+    * Created By: Mitrajsinh Gohil
+    */
     setMapView() {
         this.selectedView = 'map';
         this.currentPage = 1;
         this.updateMapMarkers();
     }
 
+    /**
+    * Method Name: navigateToRecord
+    * @description: this method is used to navigate to listing record page on click of view more
+    * Date: 17/06/2024
+    * Created By: Mitrajsinh Gohil
+    */
     navigateToRecord(event) {
         const propertyId = event.target.dataset.id;
         this[NavigationMixin.GenerateUrl]({
@@ -323,14 +377,32 @@ export default class DisplayProperties extends NavigationMixin(LightningElement)
         });
     }
 
+    /**
+    * Method Name: handleRentClick
+    * @description: this method is used to handle selected rent value
+    * Date: 25/07/2024
+    * Created By: Rachit Shah
+    */
     handleRentClick() {
         this.toggleListingType('Rent');
     }
     
+    /**
+    * Method Name: handleSaleClick
+    * @description: this method is used to handle selected sale value
+    * Date: 25/07/2024
+    * Created By: Rachit Shah
+    */
     handleSaleClick() {
         this.toggleListingType('Sale');
     }
 
+    /**
+    * Method Name: toggleListingType
+    * @description: this method is used to handle selected sale value
+    * Date: 25/07/2024
+    * Created By: Rachit Shah
+    */
     toggleListingType(listingType) {
         try {
             const index = this.filterData.listingTypes.indexOf(listingType);
@@ -340,20 +412,30 @@ export default class DisplayProperties extends NavigationMixin(LightningElement)
             } else {
                 this.filterData.listingTypes.push(listingType);
             }
-            this.applyFilters();
+            this.applyFilters(true);
         } catch (error) {
             console.error('Error toggling listing type:', error);
         }
     }
 
-    
-
+    /**
+    * Method Name: goToFirst
+    * @description: this method is used to go in first page
+    * Date: 17/06/2024
+    * Created By: Mitrajsinh Gohil
+    */
     goToFirst() {
         this.currentPage = 1;
         this.scrollToTop();
         this.updateMapMarkers();
     }
 
+    /**
+    * Method Name: goToPrevious
+    * @description: this method is used to go in preious page
+    * Date: 17/06/2024
+    * Created By: Mitrajsinh Gohil
+    */
     goToPrevious() {
         if (this.currentPage > 1) {
             this.currentPage -= 1;
@@ -362,6 +444,12 @@ export default class DisplayProperties extends NavigationMixin(LightningElement)
         }
     }
 
+    /**
+    * Method Name: goToNext
+    * @description: this method is used to go in next page
+    * Date: 17/06/2024
+    * Created By: Mitrajsinh Gohil
+    */
     goToNext() {
         if (this.currentPage < this.totalPages) {
             this.currentPage += 1;
@@ -370,12 +458,24 @@ export default class DisplayProperties extends NavigationMixin(LightningElement)
         }
     }
 
+    /**
+    * Method Name: goToLast
+    * @description: this method is used to go in last page
+    * Date: 17/06/2024
+    * Created By: Mitrajsinh Gohil
+    */
     goToLast() {
         this.currentPage = this.totalPages;
         this.scrollToTop();
         this.updateMapMarkers();
     }
 
+    /**
+    * Method Name: scrollToTop
+    * @description: this method is used to go on the top of the page
+    * Date: 17/06/2024
+    * Created By: Mitrajsinh Gohil
+    */
     scrollToTop() {
         window.scrollTo({
             top: 0,
@@ -383,6 +483,12 @@ export default class DisplayProperties extends NavigationMixin(LightningElement)
         });
     }
 
+    /**
+    * Method Name: showToast
+    * @description: this method is used to show toast message
+    * Date: 26/07/2024
+    * Created By: Mitrajsinh Gohil
+    */
     showToast(title, message, variant) {
         this.dispatchEvent(new ShowToastEvent({
             title,
@@ -390,5 +496,4 @@ export default class DisplayProperties extends NavigationMixin(LightningElement)
             variant
         }));
     }
-
 }
